3745dc96f86af434baf8edc8156cec617ed11e3c
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-04-11 13:19:11 +0200

    new function edded

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-
-let background = document.querySelector(".backgr")
-let button = document.querySelector(".button");
-let themeName = document.querySelector(".themeName");
-
-button.addEventListener("click", () => {
+{
+  const onChangeBackgroundClick = () => {
     background.classList.toggle("dark");
-    themeName.innerText = background.classList.contains("dark") ? "jasny" : "ciemny";
-});
+    themeName.innerText = background.classList.contains("dark")
+      ? "jasny"
+      : "ciemny";
+  };
 
+  let background = document.querySelector(".backgr");
+  let changeBackgroundButton = document.querySelector(".button");
+  let themeName = document.querySelector(".themeName");
+
+  changeBackgroundButton.addEventListener("click", onChangeBackgroundClick);
+}
